2037159bdf5dd7ec51ea3e7416c0ccf15532d928
Hannes Junnila <hannes.junnila@gmail.com>
2016-01-20 16:41:24 +0200

    Update to work with new mapbox-gl-native

--- index.js
+++ index.js
@@ -1,28 +1,105 @@
 var sm = new (require('sphericalmercator'))();
 var mbgl = require('mapbox-gl-native');
 var PNG = require('pngjs').PNG;
+var vtpbf = require('vt-pbf');
 var stream = require('stream');
 var concat = require('concat-stream');
+var request = require('request');
+var url = require('url');
+var fs = require('fs');
+var Pool = require('generic-pool').Pool;
+var N_CPUS = require('os').cpus().length;
 
-module.exports = function(fileSource) {
-    if (typeof fileSource.request !== 'function') throw new Error("fileSource must have a 'request' method");
-    if (typeof fileSource.cancel !== 'function') throw new Error("fileSource must have a 'cancel' method");
+mbgl.on('message', function(msg) {
+  console.log(msg);
+});
 
-    GL.prototype._fileSource = fileSource;
+function pool(style, options) {
+    return Pool({
+        create: create,
+        destroy: destroy,
+        max: N_CPUS
+    });
 
-    return GL;
-};
+    function create(callback) {
+        var map = new mbgl.Map(options);
+        var loaded = map.load(style);
+        return callback(null, map);
+    }
 
-module.exports.mbgl = mbgl;
+    function destroy(map) {
+        delete map;
+    }
+}
+
+function mbglRequest(req, callback){
+    var opts = {
+        url: req.url,
+        encoding: null,
+        gzip: true
+    };
+
+    var uri = url.parse(req.url)
+
+    if (uri.protocol === 'file:') {
+        fs.readFile(decodeURI(uri.hostname + uri.pathname), function(err, data) {
+            if (err) {
+                callback(err);
+            } else {
+                var response = {};
+                response.data = data
+                callback(null, response)
+            }
+        })
+    } else {
+      request(opts, function (err, res, body) {
+          if (err) {
+              //TODO: temporary hack to fix zero-size protobufs
+              if (err.code === "Z_BUF_ERROR") {
+                  callback(null, {data: new Buffer(0)})
+              } else {
+                  console.error(err, opts)
+                  callback(err);
+              }
+          } else if (res == undefined) {
+              callback(null, {data: new Buffer(0)})
+          } else if (res.statusCode == 200) {
+              var response = {};
+
+              if (res.headers.modified) { response.modified = new Date(res.headers.modified); }
+              if (res.headers.expires) { response.expires = new Date(res.headers.expires); }
+              if (res.headers.etag) { response.etag = res.headers.etag; }
+
+              response.data = body;
+
+              callback(null, response);
+          } else if (res.statusCode == 404) {
+              var response = {}
+
+              if (res.headers.modified) { response.modified = new Date(res.headers.modified); }
+              if (res.headers.expires) { response.expires = new Date(res.headers.expires); }
+              if (res.headers.etag) { response.etag = res.headers.etag; }
+
+              response.data = new Buffer(0);
+
+              callback(null, response)
+          } else {
+              callback(new Error(body));
+          }
+      });
+    }
+}
 
 function GL(options, callback) {
     if (!options || (typeof options !== 'object' && typeof options !== 'string')) return callback(new Error('options must be an object or a string'));
     if (!options.style) return callback(new Error('Missing GL style JSON'));
 
-    this._scale = options.scale || 1;
+    this._scale = options.query.scale || 1;
 
-    this._map = new mbgl.Map(this._fileSource);
-    this._map.load(options.style);
+    this._pool = pool(options.style, {
+      request: mbglRequest,
+      ratio: this._scale
+    });
 
     return callback(null, this);
 }
@@ -33,40 +110,56 @@ GL.registerProtocols = function(tilelive) {
 
 GL.prototype.getTile = function(z, x, y, callback) {
 
-    // Hack around tilelive API - allow params to be passed per request
-    // as attributes of the callback function.
-    var scale = callback.scale || this._scale;
+    var scale = this._scale;
 
     var bbox = sm.bbox(+x,+y,+z, false, '900913');
     var center = sm.inverse([bbox[0] + ((bbox[2] - bbox[0]) * 0.5), bbox[1] + ((bbox[3] - bbox[1]) * 0.5)]);
 
     var options = {
         // pass center in lat, lng order
-        center: [center[1], center[0]],
-        width: 512,
-        height: 512,
-        ratio: scale,
-        zoom: z
+        center: center,
+        width: 256,
+        height: 256,
+        zoom: z-1
     };
 
     this.getStatic(options, callback);
 };
 
 GL.prototype.getStatic = function(options, callback) {
-    this._map.render(options, function(err, data) {
-        if (err) return callback(err);
+    var that = this;
+    this._pool.acquire(function(err, map) {
 
-        var png = new PNG({
-            width: data.width,
-            height: data.height
+        if (err) {
+          return callback(err)
+        };
+
+        map.render(options, function(err, data) {
+
+            if (err) {
+              that._pool.release(map);
+              return callback(err)
+            };
+
+            var png = new PNG({
+                width: options.width * that._scale,
+                height: options.height * that._scale
+            });
+
+            png.data = data;
+
+            var concatStream = concat(function(buffer) {
+                return callback(null, buffer, { 'Content-Type': 'image/png' });
+            });
+
+            that._pool.release(map);
+            png.pack().pipe(concatStream);
         });
-
-        png.data = data.pixels;
-
-        var concatStream = concat(function(buffer) {
-            return callback(null, buffer, { 'Content-Type': 'image/png' });
-        });
-
-        png.pack().pipe(concatStream);
     });
 };
+
+GL.prototype.getInfo = function(callback) {
+  callback(null, {})
+}
+
+module.exports = GL;
